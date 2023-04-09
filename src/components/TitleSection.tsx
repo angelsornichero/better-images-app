3203846cb72922f1159100274d136f896acfbf0c
angelsornichero <angelspac08@gmail.com>
2023-04-09 22:54:15 +0200

    Add title section component

---
+++ src/components/TitleSection.tsx
@@ -0,0 +1,10 @@
+
+
+export default function TitleSection () {
+	return (
+		<section>
+			<h1 className='text-7xl font-bold text-center p-5 '>Better Images</h1>
+			<h3 className='text-4xl font-medium text-center p-4 text-blue-800'>Improve your images and make them lovely</h3>
+		</section>
+	)
+}
